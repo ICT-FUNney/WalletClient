8f99cfc242a50ff2ed2711143829e1da04519023
vivid344 <vivid344@icloud.com>
2019-08-05 11:19:42 +0900

    feat: connectionの状態を管理するStoreの追加

---
+++ src/actions/Connecting.js
@@ -0,0 +1,11 @@
+export const willConnect = () => {
+  return {
+    type: 'WILL_CONNECT',
+  }
+};
+
+export const doneConnect = () => {
+  return {
+    type: 'DONE_CONNECT',
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import pathReducer from './Path'
 import signInReducer from './SignIn'
 import funneyReducer from './Funney'
+import connectingReducer from "./Connecting";
 
 const reducers = combineReducers({
   pathReducer,
   signInReducer,
   funneyReducer,
+  connectingReducer,
 });
 
 export default reducers
