990d30c66e2157100f5bffbd0b4218f7bf5187a9
vivid344 <vivid344@icloud.com>
2019-08-06 10:46:31 +0900

    fix: 通信中にbuttonを押下出来ないように変更

--- src/components/Send.js
+++ src/components/Send.js
@@ -81,6 +81,7 @@ function Send() {
   const classes = useStyles();
   const dispatch = useDispatch();
   const { id } = useSelector(state => state.signInReducer);
+  const { isConnecting } = useSelector(state => state.connectingReducer);
   useEffect(() => {
     dispatch(setPath('Send'))
   });
@@ -125,6 +126,7 @@ function Send() {
         <Button
           variant="contained"
           className={classes.button}
+          disabled={isConnecting}
           onClick={()=>{
             setValue({
               ...value,
